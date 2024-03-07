d1eacfaf06b60a70af4779d0bae768f552c52e41
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-03-07 22:07:25 +0530

    [MODIFIED]:shubham This code for checking equality of two line

--- src/com/shubham/linecomparisoncomputation/LineComparison.java
+++ src/com/shubham/linecomparisoncomputation/LineComparison.java
@@ -4,24 +4,49 @@ import java.util.Scanner;
 
 public class LineComparison {
 
-	public static void main(String[] args) {
-		
-		System.out.println("Welcome To Line Camparison Computation Problem");
-		int x1;
-		int y1;
-		int x2;
-		int y2;
-		double length;
-	
+			int x1;
+			int y1;
+			int x2;
+			int y2;
+		//	double length;
+			
+		public void lineInput() {
+			
+			System.out.println("Enter the value of X1,y1,x2 and y2 co-ordinate:");
 			Scanner scanner = new Scanner(System.in);
 			x1 = scanner.nextInt();
 			y1 = scanner.nextInt();
 			x2 = scanner.nextInt();
 			y2 = scanner.nextInt();
+		}
+		
+		public double linecalculate() {
 			
-			length = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-			System.out.println("Length of Line="+length);
+			return  Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 			
+		}
+		
+		public void linecheck(LineComparison linecomparison2) {
+			if(this.linecalculate() ==  linecomparison2.linecalculate()) {
+			
+				System.out.println(" Both Lines are same");
+			}else {
+				System.out.println("Both line are not same");
+			}
+		}
+		
+	public static void main(String[] args) {
+	
+		System.out.println("Welcome To Line Camparison Computation Problem");
+		LineComparison linecomparison = new LineComparison();
+		LineComparison linecomparison2 = new LineComparison();
+
+		linecomparison.lineInput();
+		
+		linecomparison2.lineInput();
+		
+		linecomparison.linecheck(linecomparison2);
+	
 	}
 
 }
